aea881a72dc58cd11277f60582ea24c9dd3736bf
Dzmitry Maskayeu <v-dmaskayev@expedia.com>
2015-10-22 18:17:35 +0300

    Added documentation to app

--- ui.R
+++ ui.R
@@ -2,6 +2,14 @@ library(shiny)
 
 shinyUI(fluidPage(
   titlePanel("The World's Telephones"),
+
+  fluidRow(column(width = 8,"This application uses data from WorldPhones dataset.")),
+  fluidRow(column(width = 8,"Based on selected type of filter (Year or Country) it displays number of phones by check-boxed years or countries.")),
+  fluidRow(column(width = 8,"App Usage:")),  
+  fluidRow(column(width = 8,offset = 1, "1. Select filter type;")),   
+  fluidRow(column(width = 8,offset = 1, "2. Check boxes of Years or Countries you are interested in;")), 
+  fluidRow(column(width = 8,offset = 1, "3. Observe figure.")),
+  fluidRow(column(width = 8,"------------------------------------------------------------------------------------------------------------------------")),  
   
   column(3, wellPanel(
     selectInput("input_type", "Filter type",
